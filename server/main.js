69776fcbc7e57d87fb3b13bb8257774b151ef9f5
li Zuolin <Mike@lis-MacBook-Pro.local>
2016-08-15 11:02:01 -0700

    add html

--- server/main.js
+++ server/main.js
@@ -3,13 +3,18 @@ import { Email } from 'meteor/email';
 
 Meteor.startup(() => {
   // code to run on server at startup
-    process.env.MAIL_URL = 'smtp://postmaster@mikeandiana.com:e818aff6ca8db35df0b52a64fadc46d5@smtp.mailgun.org:587';
+    process.env.MAIL_URL = 'smtp://postmaster@mikeandiana.com:lzl950607@smtp.mailgun.org:587';
 });
 
 
 Meteor.methods({
     sendEmail: function(email) {
         // send the email!
-        Email.send({to:email, from:'lzlmike2@gmail.com', subject:'Thank you for signing up for our project', text:'We will share with you some news about us in a near future. See you soon!'});
+        SSR.compileTemplate('email', Assets.getText('email.html'));
+        Email.send({to:email, from:'lzlmike2@gmail.com',
+            subject:'Thank you for signing up for our project',
+            html: SSR.render('email'),
+        });
+        console.log('email sent');
     }
 });
